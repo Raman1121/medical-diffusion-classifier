5f3dc83ceb0bc40b8bfe3dc7ba8b8809abafb8e9
Raman Dutt <s2198939@daisy1.inf.ed.ac.uk>
2025-01-30 16:57:20 +0000

    CLIP bash script added

---
+++ bash_scripts/train_clip.sh
@@ -0,0 +1,21 @@
+source /raid/s2198939/miniconda3/bin/activate demm
+
+cd /raid/s2198939/medical-diffusion-classifier
+
+TRAIN_CSV="/raid/s2198939/Fundus_Images/OOD-Splits/train.csv"
+TEST_CSV="/raid/s2198939/Fundus_Images/OOD-Splits/test.csv"
+
+python train_clip.py \
+    --output_dir ./clip-roberta-finetuned \
+    --model_name_or_path ./clip-roberta \
+    --image_column path \
+    --caption_column Text \
+    --train_csv=$TRAIN_CSV \
+    --test_csv=$TEST_CSV \
+    --do_train  --do_eval \
+    --per_device_train_batch_size="64" \
+    --per_device_eval_batch_size="64" \
+    --learning_rate="5e-5" \
+    --warmup_steps="0" \
+    --weight_decay 0.1 \
+    --overwrite_output_dir \
